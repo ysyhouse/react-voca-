ac4b2f88a883dec88e6e9a73d47c0990083022f8
SunyoungYoon <ysyhouse@gmail.com>
2024-04-23 22:28:13 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Hello from './component/Hello';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Hello />
+      
+    </div>
+  );
+}
+
+export default App;
